0c7dc58244e7b86e4658c8784cf0fc7a245391eb
fuwenxue <3482606056@qq.com>
2022-08-30 12:26:43 +0800

    项目图片资源

---
+++ READMESEIF.md
@@ -0,0 +1,10 @@
+<!-- 自己添加的备注说明文件 -->
+.gitignore罗列的是忽略文件
+tsconfig.json是ts的配置文件
+src是代码文件
+    assets
+    components
+    router
+    store
+    views
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -10,14 +10,13 @@ const routes: Array<RouteConfig> = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  // 删除了脚手架的默认组件views文件夹下的About组件，也要对应的删除路由
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // 魔法注释
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = new VueRouter({
